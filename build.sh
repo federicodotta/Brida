f0c46df52cf61d5dff0bcceb936db47a93c27198
BApp Support <support@portswigger.net>
2020-05-14 10:39:45 +0100

    BApp update preparation

---
+++ build.sh
@@ -0,0 +1,7 @@
+
+#!/bin/sh
+set -ue
+
+wget https://github.com/federicodotta/RSyntaxTextArea/releases/download/2.6.1.edited/rsyntaxtextarea-2.6.1.edited.jar
+mvn install:install-file -Dfile=rsyntaxtextarea-2.6.1.edited.jar -DgroupId=com.fifesoft -DartifactId=rsyntaxtextarea -Dversion=2.6.1.edited -Dpackaging=jar
+gradle fatJar
