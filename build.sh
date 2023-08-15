6df473adcf39eef9beda2d1ef5a3425611f8a73f
PortSwigger Support <support@portswigger.net>
2023-08-15 16:38:06 +0000

    BApp Store update v0.6pre

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -ue
 
 wget https://github.com/federicodotta/RSyntaxTextArea/releases/download/2.6.1.edited/rsyntaxtextarea-2.6.1.edited.jar
-mvn install:install-file -Dfile=rsyntaxtextarea-2.6.1.edited.jar -DgroupId=com.fifesoft -DartifactId=rsyntaxtextarea -Dversion=2.6.1.edited -Dpackaging=jar
+mvn install:install-file -Dpackaging=jar -Dfile=rsyntaxtextarea-2.6.1.edited.jar -DgroupId=com.fifesoft -DartifactId=rsyntaxtextarea -Dversion=2.6.1.edited
 #gradle fatJar
 mvn package
